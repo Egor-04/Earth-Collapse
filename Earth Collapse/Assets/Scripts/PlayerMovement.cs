4b12c7229737266a656f84967903aeb956db1068
Egor-04 <egor04m@gmail.com>
2022-09-23 13:46:51 +0300

    Models and Fonts

---
+++ Earth Collapse/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private CharacterController _characterController;
+
+    private const float _minSpeed = 0f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
